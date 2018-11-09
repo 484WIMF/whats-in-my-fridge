c1cbe55f2456d92ce18e142f1f9b0aac8df2cace
Sunkyeong Lee <sunkyeong.lee@mnsu.edu>
2018-11-09 02:49:39 +0000

    with SSO Errors

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   get 'auth/twitter', :as => 'login'
 
 
-  get 'sessions/login' => 'sessions#login11'
+  get 'sessions/login' => 'sessions#login'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
