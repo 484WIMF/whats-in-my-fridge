53874102f32ebad1480792a6065c0cddf7d78cfa
Raphael <raphael.morgan@mnsu.edu>
2018-11-14 23:27:48 +0000

    Star Over

--- app/models/fridgeuser.rb
+++
@@ -1,2 +0,0 @@
-class Fridgeuser < ActiveRecord::Base
-end
--- config/initializers/omniauth.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :twitter, "fglzXeJmijsNKCyZ8QiaZPAzk", "3w2IbuloJauHVQxevnGJAgb3IK7I3llTJSOmIvwfYr2qKyAKzB"
-end
